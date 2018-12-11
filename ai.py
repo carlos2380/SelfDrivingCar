40d38ad85257a593ebbe699b450daadcf73a4dff
Carlos <carlos.2380@hotmail.com>
2018-12-11 01:54:25 +0100

    Init Network

--- ai.py
+++ ai.py
@@ -6,9 +6,19 @@ import numpy as np
 import random
 import os
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
 
+# Creating the architecture of Neural Network
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 31)
+        self.fc2 = nn.Linear(31, nb_action)
